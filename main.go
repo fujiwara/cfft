b0e0306316e8e4137b9c44edf5f92bc939797a37
fujiwara <fujiwara.shunichiro@gmail.com>
2023-12-23 00:57:37 +0900

    refactor

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ import (
 	"github.com/itchyny/gojq"
 )
 
+var Stage = types.FunctionStageDevelopment
+
 type CFFT struct {
 	cloudfront   *cloudfront.Client
 	functionCode []byte
@@ -27,13 +29,13 @@ type CFFT struct {
 type TestCase struct {
 	Event  []byte
 	Expect any
-	Ignore string
+	Ignore *gojq.Query
 }
 
 type CLI struct {
-	Name     string `arg:"" help:"function name" required:"true"`
-	Function string `arg:"" help:"function code file" required:"true"`
-	Event    string `arg:"" help:"event object file" required:"true"`
+	Name     string `arg:"" help:"function name"`
+	Function string `arg:"" help:"function code file"`
+	Event    string `arg:"" help:"event object file"`
 	Expect   string `arg:"" help:"expect object file" optional:"true"`
 	Ignore   string `short:"i" long:"ignore" help:"ignore fields in the expect object by jq syntax"`
 }
@@ -63,6 +65,7 @@ func Run(ctx context.Context) error {
 	if err := json.Unmarshal(testCase.Event, &eventObject); err != nil {
 		return fmt.Errorf("failed to parse event object, %w", err)
 	}
+
 	if cli.Expect != "" {
 		expectBytes, err := os.ReadFile(cli.Expect)
 		if err != nil {
@@ -72,50 +75,77 @@ func Run(ctx context.Context) error {
 			return fmt.Errorf("failed to parse expect object, %w", err)
 		}
 	}
-	testCase.Ignore = cli.Ignore
+
+	if cli.Ignore != "" {
+		q, err := gojq.Parse(cli.Ignore)
+		if err != nil {
+			return fmt.Errorf("failed to parse ignore query, %w", err)
+		}
+		testCase.Ignore = q
+	}
+
 	app.testCases = append(app.testCases, testCase)
 
 	return app.TestFunction(ctx, cli.Name)
 }
 
 func (app *CFFT) TestFunction(ctx context.Context, name string) error {
-	var created bool
-	var etag string
+	etag, err := app.prepareFunction(ctx, name)
+	if err != nil {
+		return fmt.Errorf("failed to prepare function, %w", err)
+	}
+
+	for _, testCase := range app.testCases {
+		if err := app.runTestCase(ctx, name, etag, testCase); err != nil {
+			return fmt.Errorf("failed to run test case, %w", err)
+		}
+	}
+	return nil
+}
+
+func (app *CFFT) createFunction(ctx context.Context, name string, code []byte) (string, error) {
+	log.Printf("[info] creating function %s...", name)
+	res, err := app.cloudfront.CreateFunction(ctx, &cloudfront.CreateFunctionInput{
+		Name:         aws.String(name),
+		FunctionCode: code,
+		FunctionConfig: &types.FunctionConfig{
+			Comment: aws.String("created by cfft"),
+			Runtime: types.FunctionRuntimeCloudfrontJs20,
+		},
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to create function, %w", err)
+	}
+	log.Printf("[info] function %s created", name)
+	return aws.ToString(res.ETag), nil
+}
+
+func (app *CFFT) prepareFunction(ctx context.Context, name string) (string, error) {
 	var functionConfig *types.FunctionConfig
+	var etag string
+
 	res, err := app.cloudfront.DescribeFunction(ctx, &cloudfront.DescribeFunctionInput{
-		Name: aws.String(name),
+		Name:  aws.String(name),
+		Stage: Stage,
 	})
 	if err != nil {
 		var notFound types.EntityNotFound
 		if errors.Is(err, &notFound) {
-			return fmt.Errorf("failed to describe function, %w", err)
+			return "", fmt.Errorf("failed to describe function, %w", err)
 		}
-		log.Printf("[info] function %s not found. create function", name)
-		res, err := app.cloudfront.CreateFunction(ctx, &cloudfront.CreateFunctionInput{
-			Name:         aws.String(name),
-			FunctionCode: app.functionCode,
-			FunctionConfig: &types.FunctionConfig{
-				Comment: aws.String("created by cfft"),
-				Runtime: types.FunctionRuntimeCloudfrontJs20,
-			},
-		})
+		log.Printf("[info] function %s not found", name)
+		etag, err = app.createFunction(ctx, name, app.functionCode)
 		if err != nil {
-			return fmt.Errorf("failed to create function, %w", err)
+			return "", fmt.Errorf("failed to create function, %w", err)
 		}
-		log.Printf("[info] function %s created", name)
-		etag = aws.ToString(res.ETag)
-		created = true
 	} else {
 		log.Printf("[info] function %s found", name)
-		etag = aws.ToString(res.ETag)
 		functionConfig = res.FunctionSummary.FunctionConfig
-	}
-
-	if !created {
 		if res, err := app.cloudfront.GetFunction(ctx, &cloudfront.GetFunctionInput{
-			Name: aws.String(name),
+			Name:  aws.String(name),
+			Stage: Stage,
 		}); err != nil {
-			return fmt.Errorf("failed to describe function, %w", err)
+			return "", fmt.Errorf("failed to describe function, %w", err)
 		} else {
 			etag = aws.ToString(res.ETag)
 			if bytes.Equal(res.FunctionCode, app.functionCode) {
@@ -129,61 +159,62 @@ func (app *CFFT) TestFunction(ctx context.Context, name string) error {
 					FunctionConfig: functionConfig,
 				})
 				if err != nil {
-					return fmt.Errorf("failed to update function, %w", err)
+					return "", fmt.Errorf("failed to update function, %w", err)
 				}
 				etag = aws.ToString(res.ETag)
 			}
 		}
 	}
+	return etag, nil
+}
 
-	for _, testCase := range app.testCases {
-		var failed bool
-		res, err := app.cloudfront.TestFunction(ctx, &cloudfront.TestFunctionInput{
-			Name:        aws.String(name),
-			IfMatch:     aws.String(etag),
-			Stage:       types.FunctionStageDevelopment,
-			EventObject: testCase.Event,
-		})
-		if err != nil {
-			return fmt.Errorf("failed to test function, %w", err)
-		}
-		if errMsg := aws.ToString(res.TestResult.FunctionErrorMessage); errMsg != "" {
-			log.Printf("[error] %s", errMsg)
-			failed = true
-		}
-		log.Printf("[info] ComputeUtilization:%s", aws.ToString(res.TestResult.ComputeUtilization))
-		for _, l := range res.TestResult.FunctionExecutionLogs {
-			log.Println(l)
-		}
-		fmt.Println(aws.ToString(res.TestResult.FunctionOutput))
-		if failed {
-			return errors.New("test failed")
-		}
-		if testCase.Expect == nil {
-			continue
-		}
+func (app *CFFT) runTestCase(ctx context.Context, name, etag string, c TestCase) error {
+	res, err := app.cloudfront.TestFunction(ctx, &cloudfront.TestFunctionInput{
+		Name:        aws.String(name),
+		IfMatch:     aws.String(etag),
+		Stage:       Stage,
+		EventObject: c.Event,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to test function, %w", err)
+	}
+	var failed bool
+	if errMsg := aws.ToString(res.TestResult.FunctionErrorMessage); errMsg != "" {
+		log.Printf("[error] %s", errMsg)
+		failed = true
+	}
+	log.Printf("[info] ComputeUtilization:%s", aws.ToString(res.TestResult.ComputeUtilization))
+	for _, l := range res.TestResult.FunctionExecutionLogs {
+		log.Println(l)
+	}
+	fmt.Println(aws.ToString(res.TestResult.FunctionOutput))
+	if failed {
+		return errors.New("test failed")
+	}
+	if c.Expect == nil {
+		return nil
+	}
 
-		var rhs any
-		if err := json.Unmarshal([]byte(*res.TestResult.FunctionOutput), &rhs); err != nil {
-			return fmt.Errorf("failed to parse function output, %w", err)
-		}
-		var options []jsondiff.Option
-		if testCase.Ignore != "" {
-			q, err := gojq.Parse(testCase.Ignore)
-			if err != nil {
-				return fmt.Errorf("failed to parse ignore query, %w", err)
-			}
-			options = append(options, jsondiff.Ignore(q))
-		}
-		diff, err := jsondiff.DiffFromObjects(testCase.Expect, rhs, options...)
-		if err != nil {
-			return fmt.Errorf("failed to diff, %w", err)
-		}
-		if diff != "" {
-			return fmt.Errorf("expect and actual are not equal:\n%s", diff)
-		} else {
-			log.Println("[info] expect and actual are equal")
-		}
+	var rhs any
+	if err := json.Unmarshal([]byte(*res.TestResult.FunctionOutput), &rhs); err != nil {
+		return fmt.Errorf("failed to parse function output, %w", err)
+	}
+	var options []jsondiff.Option
+	if c.Ignore != nil {
+		options = append(options, jsondiff.Ignore(c.Ignore))
+	}
+	diff, err := jsondiff.Diff(
+		&jsondiff.Input{Name: "expect", X: c.Expect},
+		&jsondiff.Input{Name: "actual", X: rhs},
+		options...,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to diff, %w", err)
+	}
+	if diff != "" {
+		return fmt.Errorf("expect and actual are not equal:\n%s", diff)
+	} else {
+		log.Println("[info] expect and actual are equal")
 	}
 	return nil
 }
