853bdcecf5a697c73773ee8a51e46ce65a96cd5d
fujiwara <fujiwara.shunichiro@gmail.com>
2024-02-28 18:17:18 +0900

    Support to render event or expect object.

--- README.md
+++ README.md
@@ -503,13 +503,29 @@ Before publishing the function, you need to run `cfft diff` to check the differe
 
 ### Render function code
 
-`cfft render` renders the function code to stdout.
+`cfft render` renders the function code or event object or expect object to STDOUT.
+
+```
+Usage: cfft render [<target>]
+
+render function code
+
+Arguments:
+  [<target>]    render target (function,event,expect)
+
+Flags:
+       --test-case=""          test case name (for target event or expect)
+```
 
 ```console
 $ cfft render
 ```
 
 You can use `cfft render` to check the function code after rendering the template syntax.
+
+`cfft render event --test-case=foo` renders the event object of the test case named 'foo'.
+
+The `--test-case` flag is available only for the `event` and `expect` targets. If `--test-case` is not specified, cfft renders the event or expect object of first test case.
 
 ## Template syntax
 
--- render.go
+++ render.go
@@ -3,12 +3,26 @@ package cfft
 import (
 	"context"
 	"fmt"
+	"log/slog"
 )
 
 type RenderCmd struct {
+	Target   string `arg:"" help:"render target (function,event,expect)" default:"function" enum:"function,event,expect"`
+	TestCase string `cmd:"" help:"test case name (for target event or expect)" default:""`
 }
 
 func (app *CFFT) Render(ctx context.Context, opt *RenderCmd) error {
+	switch opt.Target {
+	case "function", "": // default target
+		return app.renderFunction(ctx)
+	case "event", "expect":
+		return app.renderTestCase(ctx, opt)
+	default:
+		return fmt.Errorf("invalid target %s", opt.Target)
+	}
+}
+
+func (app *CFFT) renderFunction(ctx context.Context) error {
 	localCode, err := app.config.FunctionCode(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to read function code, %w", err)
@@ -17,4 +31,27 @@ func (app *CFFT) Render(ctx context.Context, opt *RenderCmd) error {
 		return fmt.Errorf("failed to write function code into STDOUT, %w", err)
 	}
 	return nil
+}
+
+func (app *CFFT) renderTestCase(ctx context.Context, opt *RenderCmd) error {
+	for _, tc := range app.config.TestCases {
+		// if test case name is empty, render first test case
+		if tc.Name == opt.TestCase || opt.TestCase == "" {
+			var b []byte
+			switch opt.Target {
+			case "event":
+				b = tc.EventBytes()
+			case "expect":
+				b = tc.ExpectBytes()
+			default:
+				return fmt.Errorf("invalid target %s", opt.Target)
+			}
+			slog.Info(f("rendering %s of test case %s", opt.Target, tc.Name))
+			if _, err := app.stdout.Write(b); err != nil {
+				return fmt.Errorf("failed to write %s into STDOUT, %w", opt.Target, err)
+			}
+			return nil
+		}
+	}
+	return fmt.Errorf("test case %s not found", opt.TestCase)
 }
